f594483269fc68b456658df000464744b8724bf6
Arsen Rogovoi <arrog13@gmail.com>
2024-03-15 16:08:44 +0200

    started buildin expert profile page

--- src/pages/ExpertProfilePage.jsx
+++ src/pages/ExpertProfilePage.jsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import experts from "../mockData/experts.json";
+import { Container, Grid, Typography } from "@mui/material";
+import Schedule from "./scheduleComponents/Schedule";
 
 const ExpertProfilePage = () => {
   let { expertId } = useParams();
-  // Здесь вы можете использовать expertId, чтобы загрузить данные профиля эксперта
-  return <div>Профиль эксперта {expertId}</div>;
+  const [expert, setExpert] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      // change array method!
+      experts.map((expert) => {
+        if (expert.id === expertId) {
+          setExpert(expert);
+        }
+      });
+    }, 1000);
+  }, []);
+
+  return expert ? (
+    <Container>
+      {/* photo and name */}
+      <Grid container mt={1} justifyContent={"space-between"}>
+        <Grid item xs={3}>
+          <img
+            src={expert.photoUrl}
+            alt={`${expert.name}`}
+            width={"100%"}
+            style={{
+              borderRadius: "5px",
+            }}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="h6">{expert.name}</Typography>
+        </Grid>
+      </Grid>
+      {/* About expert */}
+      <Grid container mt={1}>
+        <Grid item xs={12}>
+          <Typography variant="h5">About the expert</Typography>
+          <Typography mt={1}>{expert.description}</Typography>
+        </Grid>
+      </Grid>
+      {/* Schedule */}
+      <Schedule />
+    </Container>
+  ) : (
+    <>loading or missing data</>
+  );
 };
 
 export default ExpertProfilePage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -13,12 +13,11 @@ const Router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={ROUTES.ROOT.path} element={<GlobalLayout />}>
       <Route index element={<EntryPage />} />
-      <Route path={ROUTES.EXPERTS.path} element={<ExpertSearchPage />}>
-        <Route
-          path={ROUTES.EXPERT_PROFILE.path}
-          element={<ExpertProfilePage />}
-        />
-      </Route>
+      <Route path={ROUTES.EXPERTS.path} element={<ExpertSearchPage />} />
+      <Route
+        path={ROUTES.EXPERT_PROFILE.path}
+        element={<ExpertProfilePage />}
+      />
       <Route path={ROUTES.ABOUT.path} element={<>about</>} />
       <Route path={ROUTES.LOGIN.path} element={<>login page</>} />
       <Route path={ROUTES.SIGNUP.path} element={<>sign up page</>} />
