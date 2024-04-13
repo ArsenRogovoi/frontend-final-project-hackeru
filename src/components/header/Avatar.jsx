8112fb83fc6d6c1b1f5779c5c42c84865141ad9f
Arsen Rogovoi <arrog13@gmail.com>
2024-04-13 22:36:01 +0300

    UserContext provided in GlobalLayout component. Logout logic added.

--- src/components/header/Avatar.jsx
+++ src/components/header/Avatar.jsx
@@ -1,14 +1,54 @@
-import { Avatar as MuiAvatar } from "@mui/material";
+import { IconButton, Menu, MenuItem, Avatar as MuiAvatar } from "@mui/material";
 import { deepPurple } from "@mui/material/colors";
+import { useState } from "react";
+import { useUser } from "../../contexts/UserContext";
 
 const Avatar = ({ sx, user }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { logoutUser } = useUser();
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    logoutUser();
+    handleClose();
+  };
+
   const {
     username: { firstName, lastName },
   } = user;
+
   return (
-    <MuiAvatar
-      sx={{ ...sx, bgcolor: deepPurple[500] }}
-    >{`${firstName[0]}${lastName[0]}`}</MuiAvatar>
+    <>
+      <IconButton
+        aria-controls={open ? "profile-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <MuiAvatar
+          id="profile-avatar"
+          sx={{ ...sx, bgcolor: deepPurple[500] }}
+        >{`${firstName[0]}${lastName[0]}`}</MuiAvatar>
+      </IconButton>
+      <Menu
+        id="profile-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
   );
 };
 
--- src/components/layouts/GlobalLayout.jsx
+++ src/components/layouts/GlobalLayout.jsx
@@ -3,10 +3,21 @@ import PageFooter from "./PageFooter";
 import PageHeader from "../header/PageHeader";
 import { Outlet, useLocation } from "react-router-dom";
 import RouteContext from "../../contexts/RouteContext";
+import { useUser } from "../../contexts/UserContext";
+import { getToken } from "../../utils/localStorageService";
+import { isTokenExpired } from "../../utils/jwtService";
 
 const GlobalLayout = () => {
   const location = useLocation();
+  const { getUserData } = useUser();
 
+  const checkAuth = () => {
+    const token = getToken();
+    const isExpired = isTokenExpired(token);
+    if (!isExpired) getUserData();
+  };
+
+  checkAuth();
   return (
     <RouteContext.Provider value={{ currentPath: location.pathname }}>
       <Grid container direction={"column"} sx={{ minHeight: "100vh" }}>
